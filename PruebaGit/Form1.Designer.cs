4760a3d84d0871d0cdfbe2f3ec9d58afe642eda9
LENOVO <LENOVO@PASTELITO>
2025-05-10 15:56:50 -0500

    Apellido

--- PruebaGit/Form1.Designer.cs
+++ PruebaGit/Form1.Designer.cs
@@ -32,11 +32,14 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(183, 168);
+            this.button1.Location = new System.Drawing.Point(183, 205);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(89, 30);
             this.button1.TabIndex = 0;
@@ -59,22 +62,52 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(123, 22);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(342, 117);
+            this.listBox1.Location = new System.Drawing.Point(312, 117);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(199, 196);
             this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Apellido";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(149, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(123, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(531, 117);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(202, 196);
+            this.listBox2.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 418);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -91,6 +124,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- PruebaGit/Form1.cs
+++ PruebaGit/Form1.cs
@@ -19,18 +19,37 @@ namespace PruebaGit
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string nombre = textBox1.Text.Trim(); // Obtener texto sin espacios al inicio/final
+            string nombre = textBox1.Text.Trim(); 
+            string apellido = textBox2.Text.Trim(); 
 
-            if (!string.IsNullOrEmpty(nombre)) // Validar que no esté vacío
+
+            if (!string.IsNullOrEmpty(nombre)) 
             {
-                listBox1.Items.Add(nombre); // Agregar a la ListBox
-                textBox1.Clear(); // Limpiar el TextBox
-                textBox1.Focus(); // Regresar el foco al TextBox
+                listBox1.Items.Add(nombre); 
+                textBox1.Clear(); 
+                textBox1.Focus(); 
             }
             else
             {
                 MessageBox.Show("Por favor, ingresa un nombre.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+
+            if (!string.IsNullOrEmpty(apellido)) 
+            {
+                listBox2.Items.Add(apellido);
+                textBox2.Clear(); 
+                textBox2.Focus(); 
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un nombre.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
